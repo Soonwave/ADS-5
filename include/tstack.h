07fc8edf2418749e01cacb201ebb8da9e0a50763
Soonwave <evawnoos@bk.ru>
2026-04-13 22:27:30 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,25 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[SIZE];
+    int head;
+public:
+    TStack() : head(-1) {}
+    bool isEmpty() const { return head == -1; }
+    bool isFull()  const { return head == SIZE - 1; }
+
+    void push(const T& item) {
+        if (!isFull()) data[++head] = item;
+    }
+
+    void pop() {
+        if (!isEmpty()) head--;
+    }
+
+    T top() const {
+        return !isEmpty() ? data[head] : T();
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
